d68590fecfc94c7e28460982af1e172b390f233c
ML510 <mr.mahesh.303@gmail.com>
2026-08-05 15:17:27 +0530

    Serializer Update

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -12,17 +12,26 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
         
-# class UserProfileSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = UserProfile
-#         fields = '__all__'
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
         
-# class OrderSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Order
-#         fields = '__all__'
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
         
-# class OrderItemSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = OrderItem
-#         fields = '__all__'
+class OrderSerializer(serializers.ModelSerializer):
+
+    user = serializers.StringRelatedField(read_only=True)
+
+    items = OrderItemSerializer(
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = Order
+        fields = '__all__'
