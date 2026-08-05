8cd211170e1277d5c720850db790d9ec9af18342
ML510 <mr.mahesh.303@gmail.com>
2026-08-05 16:07:58 +0530

    Create url path And Get funshions

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,11 +1,28 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Category, Order, Product, UserProfile
+from .serializers import CategorySerializer, ProductSerializer, UserProfileSerializer, OrderSerializer
 
-def home(request): 
-    data = {
-        'message':'Welcome to the E-Commerce store'
-    }
-    
-    return JsonResponse(data)
+@api_view(['GET'])
+def get_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
+@api_view(['GET'])
+def get_categories(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
 
-# Create your views here.
+@api_view(['GET'])
+def get_user_profile(request):
+    user_profiles = UserProfile.objects.all()
+    serializer = UserProfileSerializer(user_profiles, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_orders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
